95b7d13e414752e9bd7634bf6b6af49f4a7b1d0a
Lissy Duncan <duncanlissy@gmail.com>
2024-06-08 10:29:43 +1000

    introducing while loop

---
+++ while-loop.py
@@ -0,0 +1,24 @@
+# Counting coins in Piggy bank
+
+# Do not practice this idea
+no_of_coins = 23
+
+# print("Counted 1!")
+# no_of_coins -= 1
+
+# print("Counted 1!")
+# no_of_coins -= 1
+
+# print("Counted 1!")
+# no_of_coins -= 1
+
+# print("Counted 1!")
+# no_of_coins -= 1
+
+# print("Counted 1!")
+# no_of_coins -= 1
+
+
+# Better Implementation
+while no_of_coins > 0:
+    print("Counted 1!")
